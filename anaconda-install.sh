18d62615a9e92d6721df52783c115fada66cfdfb
Rodrigo Ribeiro <45922185+Ribeiro-R@users.noreply.github.com>
2020-09-16 22:51:43 -0300

    Delete anaconda-install.sh

--- anaconda-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update -qq
-sudo apt-get upgrade -yy
-sudo apt-get install -yy libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2\
-libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
